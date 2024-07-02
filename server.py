a53df54197e27b200e8f4994bfd5a5c6b3200520
Michael Romero <romerom@moravian.edu[core]>
2024-07-01 20:09:25 -0400

    We got an error in the id_info part so im trying to comment stuff out to test

--- server.py
+++ server.py
@@ -71,7 +71,7 @@ def callback():
     token_request = google.auth.transport.requests.Request(session=cached_session)
 
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials.id_token,
+        #id_token=credentials.id_token,
         request=token_request,
         audience=GOOGLE_CLIENT_ID)
 
